1a247accf21903d2288179a092f37c0ee65f7865
Andrii <andrii.paziuk@gmail.com>
2023-08-10 13:34:28 -0230

    Adding secutiry groups and key_pair, also adding ansible to the project

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -2,12 +2,24 @@ resource "aws_vpc" "Project_ec2_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_security_group" "project_sec_group" {
+  name        = "project_sec_group"
+  description = "project_sec_group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] // Allow SSH from anywhere
+  }
+}
 
-resource "aws_instance" "Project_ec2" {
-  count         = var.worker_count
-  ami           = "ami-0f34c5ae932e6f0e4"
-  instance_type = "t2.micro"
+resource "aws_instance" "project_ec2" {
+  count                  = var.worker_count
+  ami                    = "ami-0f34c5ae932e6f0e4"
+  instance_type          = "t2.micro"
+  key_name               = "terraform-key"
+  vpc_security_group_ids = [aws_security_group.project_sec_group.id]
   tags = {
-    Name = "Project_ec2"
+    Name = "project_ec2"
   }
 }
--- terraform_project/provider.tf
+++ terraform_project/provider.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region     = var.aws_region
   access_key = "AKIAT4NPDXM4EVKBO6GD"
   secret_key = "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"
+
 }
 
 provider "kubernetes" {
