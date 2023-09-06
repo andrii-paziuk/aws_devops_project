57aa82c8483e78ddf403da356057a2c1d1cfad72
Andrii <andrii.paziuk@gmail.com>
2023-09-06 11:15:47 -0230

    Changing name of the folder

--- terraform_project/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS region for the cluster"
-  default     = "us-east-1"
-}
-
-variable "worker_count" {
-  description = "Number of worker nodes"
-  default     = 2
-}
